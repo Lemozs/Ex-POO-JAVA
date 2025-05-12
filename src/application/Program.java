a6c814ff3e38fb79c70f05907e425d500a7ab31d
Lemozs <lemosz0612@gmail.com>
2025-05-12 16:30:14 -0300

    encapsulation

--- src/application/Program.java
+++ src/application/Program.java
@@ -10,23 +10,25 @@ public class Program {
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
+
+		Product p = new Product();
 		
 		System.out.println("Enter product data: ");
 		System.out.print("Name: ");
         String name = sc.nextLine();
 		System.out.print("Price: ");
 		double price = sc.nextDouble();
-		System.out.print("Quantity: ");
-		int quantity = sc.nextInt();
-
-		Product product = new Product(name, price, quantity);
+		Product product = new Product(name, price);
+		
+		System.out.println("Updated name: " + product.getName());
+		System.out.println("Updated price: " + product.getPrice());
 		
 		System.out.println();
 		System.out.println(product);
 
 		System.out.println();
 		System.out.print("Enter the number of products to be added in stock: ");
-		quantity = sc.nextInt();
+		int quantity = sc.nextInt();
 		product.addProducts(quantity);
 
 		System.out.println();
--- src/entities/Product.java
+++ src/entities/Product.java
@@ -2,9 +2,15 @@ package entities;
 
 public class Product {
 
-	public String name;
-	public double price;
-	public int quantity;
+	private String name;
+	private double price;
+	private int quantity;
+	
+	/////////////////////////////////////////
+	
+	public Product() {
+		
+	}
 	
 	public Product(String name, double price, int quantity) {
 		super();
@@ -12,7 +18,37 @@ public class Product {
 		this.price = price;
 		this.quantity = quantity;
 	}
+	
+	public Product(String name, double price) {
+		super();
+		this.name = name;
+		this.price = price;
+	}
+	
+	/////////////////////////////////////////
 
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	/////////////////////////////////////////
+	
 	public double totalValueInStock() {
 		return price * quantity;
 	}
@@ -26,6 +62,8 @@ public class Product {
 		this.quantity -= quantity;
 	}
 
+	/////////////////////////////////////////
+	
 	@Override
 	public String toString() {
 		return "Product name: " + name + ", price: $" + price + ", quantity: " + quantity + ", total: $"
