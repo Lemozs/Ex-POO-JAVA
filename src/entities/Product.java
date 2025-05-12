43e9a36f0231f079084bf3d1bfed50e9c0258a6a
Lemozs <lemosz0612@gmail.com>
2025-05-12 14:06:42 -0300

    constructors

--- src/entities/Product.java
+++ src/entities/Product.java
@@ -5,23 +5,31 @@ public class Product {
 	public String name;
 	public double price;
 	public int quantity;
+	
+	public Product(String name, double price, int quantity) {
+		super();
+		this.name = name;
+		this.price = price;
+		this.quantity = quantity;
+	}
 
 	public double totalValueInStock() {
 		return price * quantity;
 	}
-	
-	public void addProducts(int quantity ) {
+
+	public void addProducts(int quantity) {
 		this.quantity += quantity;
-		
+
 	}
-	
+
 	public void removeProducts(int quantity) {
 		this.quantity -= quantity;
 	}
-	
+
 	@Override
 	public String toString() {
-		return "Product name: " + name + ", price: $" + price + ", quantity: " + quantity + ", total: $" + totalValueInStock();
+		return "Product name: " + name + ", price: $" + price + ", quantity: " + quantity + ", total: $"
+				+ totalValueInStock();
 	}
-	
+
 }
